a24e0f5310fa61d3e961d713c059955e60f080ab
jkL970910 <jkliu97@gmail.com>
2022-12-12 22:39:02 -0500

    disable github page

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <BrowserRouter basename={window.location.pathname || ''}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ApolloProvider>

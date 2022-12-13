798a195fbbb7f54c59aa57033656bad8eb00f23e
jkL970910 <jkliu97@gmail.com>
2022-12-12 22:28:13 -0500

    add basename

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <BrowserRouter>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <App />
       </BrowserRouter>
     </ApolloProvider>
